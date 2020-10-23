2c94f07a8f165f1fc1933ff8820fc9fc3614acb8
lenovo <lenovo@LAPTOP-0LME9BJ7>
2020-10-23 10:28:49 +0100

    LED PORTC out put

--- main.s
+++ main.s
@@ -6,8 +6,8 @@ main:
 	org 0x0
 	goto	setup
 	
-	org 0x100		    ; Main code starts here at address 0x100
-
+	org 0x100		    ; Main code starts here at address 0x10
+	
 	; ******* Programme FLASH read Setup Code ****  
 setup:	
 	bcf	CFGS	; point to Flash program memory  
@@ -15,12 +15,14 @@ setup:
 	goto	start
 	; ******* My data and where to put it in RAM *
 myTable:
-	db	'T','h','i','s',' ','i','s',' ','j','u','s','t'
-	db	' ','s','o','m','e',' ','d','a','t','a'
+	db	'1','2','3','4','5','6','7','8','9'
+	db	'1'
 	myArray EQU 0x400	; Address in RAM for data
 	counter EQU 0x10	; Address of counter variable
 	; ******* Main programme *********************
 start:	
+	movlw	0x0
+	movwf	TRISC, A
 	lfsr	0, myArray	; Load FSR0 with address in RAM	
 	movlw	low highword(myTable)	; address of data in PM
 	movwf	TBLPTRU, A	; load upper bits to TBLPTRU
@@ -33,9 +35,12 @@ start:
 loop:
         tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
 	movff	TABLAT, POSTINC0	; move read data from TABLAT to (FSR0), increment FSR0	
+	movf	0, W, A
+	movwf	PORTC, A
 	decfsz	counter, A	; count down to zero
 	bra	loop		; keep going until finished
 	
 	goto	0
 
 	end	main
+
