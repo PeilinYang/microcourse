3ddd5cbb6f2c8174ce3b0b212c564c5be4b4ed71
PeilinYang <>
2020-10-23 14:59:17 +0100

    LED Flash sequence using port C as output. Implemented buttons (portD) that set flash frequency and stop the program if bit 7 is pressed.

--- main.s
+++ main.s
@@ -15,12 +15,23 @@ setup:
 	goto	start
 	; ******* My data and where to put it in RAM *
 myTable:
-	db	'1','2','3','4','5','6','7','8','9'
+	db	'1','a','3','b','5','c','7','8','9'
 	db	'1'
 	myArray EQU 0x400	; Address in RAM for data
 	counter EQU 0x10	; Address of counter variable
+ delay: decfsz	0x20, 1
+	bra delay
+	decfsz	0x22,1
+	bra delay
+	decfsz	0x24, 1
+	bra delay
+	return
 	; ******* Main programme *********************
 start:	
+	movlw  0xff		    ;Implementing program to pause/unpause the LED Flashes
+	movwf  TRISD,A             ; Set PORTD to inputs
+	movlw	0x80
+	movwf	0x06,A
 	movlw	0x0
 	movwf	TRISC, A
 	lfsr	0, myArray	; Load FSR0 with address in RAM	
@@ -30,12 +41,21 @@ start:
 	movwf	TBLPTRH, A	; load high byte to TBLPTRH
 	movlw	low(myTable)	; address of data in PM
 	movwf	TBLPTRL, A	; load low byte to TBLPTRL
-	movlw	22		; 22 bytes to read
+	movlw	10		; 22 bytes to read
 	movwf 	counter, A	; our counter register
 loop:
+	movf	PORTD, W, A	    ; moving input value into W
+	cpfsgt	0x06		    ;if any button is pressed, then program is paused.
+	bra	loop
+	;moving PORTD to the counters, so speed can be changed using buttons		;movlw	0x10
+	movwf	0x20, A
+	movwf	0x22, A
+	movwf	0x24, A
+	call	delay
         tblrd*+			; move one byte from PM to TABLAT, increment TBLPRT
 	movff	TABLAT, POSTINC0	; move read data from TABLAT to (FSR0), increment FSR0	
-	movf	0, W, A
+	movf	TABLAT, W, A
+	clrf	PORTC, A
 	movwf	PORTC, A
 	decfsz	counter, A	; count down to zero
 	bra	loop		; keep going until finished
